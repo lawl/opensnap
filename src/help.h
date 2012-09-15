7af14168ca7a422fd5b9797402f5c636b7a0a9d8
Ikem Krueger <ikem.krueger@googlemail.com>
2012-09-15 18:43:07 +0200

    changed version to 0.9.0

--- src/help.h
+++ src/help.h
@@ -6,10 +6,10 @@ void printHelp(){
     printf("  -d, --daemon                     run opensnap as daemon\n");
     printf("  -o, --offset <PIXEL>             set offset in pixel\n");
     printf("  -t, --threshold <MSECONDS>       set threshold in microseconds\n");
-    printf("  -s, --script <PATH>              set your script path to <PATH>\n");
+    printf("  -s, --script <PATH>              set script path to <PATH>\n");
     printf("  -v, --verbose                    be verbose\n");
-    printf("  -h, --help                       print this help\n");
     printf("  -V, --version                    show version\n");
+    printf("  -h, --help                       print this help\n");
     printf("\n");
     printf("Example:\n");
     printf("\n");
--- src/version.h
+++ src/version.h
@@ -1,6 +1,6 @@
 int Major=0;
-int Minor=8;
-int Revision=2;
+int Minor=9;
+int Revision=0;
 
 void printVersion(){
     printf("Opensnap version %d.%d.%d\n", Major, Minor, Revision);
