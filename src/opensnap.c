8cdbdce85d96f69b4d636cb94b4474504cafe4bd
Zack Drach <hogbait@gmail.com>
2013-12-24 02:07:45 -0500

    Windows don't snap when grabbed near screen edge.

--- src/opensnap.c
+++ src/opensnap.c
@@ -90,6 +90,11 @@ int main(int argc, char **argv)
         if(verbose)
             printf("Mouse Coordinates: %d %d %d\n", mousepos.x, mousepos.y, mousepos.state );
         if((LEFTCLICK & mousepos.state)==LEFTCLICK){
+            if(!isdrag && isinitialclick) {
+                if(isTitlebarHit(dsp, &mousepos)){
+                    isdrag=1;
+                }
+            }
             if(relativeMousepos.y<=offset)
                 action=HIT_TOP;
             else if(relativeMousepos.x<=offset)
@@ -98,14 +103,8 @@ int main(int argc, char **argv)
                 action=HIT_RIGHT;
             else if(relativeMousepos.y>=scrinfo.screens[scrnn].height-offset-1)
                 action=HIT_BOTTOM;
-            else {
-                if(!isdrag && isinitialclick) {
-                    if(isTitlebarHit(dsp, &mousepos)){
-                        isdrag=1;
-                    }
-                }
+            else
                 action=0;
-            }
             isinitialclick=false;
         }
         if(verbose)printf("action is: %d, isdrag is: %d\n",action,isdrag);
